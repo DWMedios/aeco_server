e6d7bd4be4b51b54f1e97dd04235f825bb7661ee
franciscocamposchimal <camposfranciscoing@gmail.com>
2025-04-30 12:02:16 -0600

    fix: company seeder

--- db/seeders/1727731819942-CreateCompanySeeder.ts
+++ db/seeders/1727731819942-CreateCompanySeeder.ts
@@ -1,7 +1,6 @@
 import { MigrationInterface, QueryRunner } from 'typeorm'
 
 const companyTable = 'companies'
-const companySettingsTable = 'company_settings'
 const companyRFC = 'XAXX010101012'
 
 export class CreateCompanySeeder1727731819942 implements MigrationInterface {
@@ -11,34 +10,11 @@ export class CreateCompanySeeder1727731819942 implements MigrationInterface {
         VALUES ('AECO Company', '${companyRFC}', 'Jalisco', 'Guadalajara', 'Av. Vallarta 6503', '45020', '9994566778', 
         '{"name": "Juan Perez", "email": "elrepresentante@aeco.com", "phone": "9994566778", "position": "CEO"}')`,
     )
-
-    const getCompanyByRfc = await queryRunner.query(
-      `SELECT * FROM ${companyTable} WHERE rfc = '${companyRFC}' LIMIT 1`,
-    )
-
-    const companyId = getCompanyByRfc[0]?.id
-
-    if (!companyId) console.error('Company not found')
-
-    await queryRunner.query(
-      `INSERT INTO ${companySettingsTable} ("key", metadata, "companyId") 
-        VALUES ('71402166-9e0a-47d2-851a-322381b4c1cd', '{"defaultCurrency": "USD"}', ${companyId})`,
-    )
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    const result = await queryRunner.query(
-      `SELECT id FROM ${companyTable} WHERE rfc = '${companyRFC}' LIMIT 1`,
-    )
-    const companyId = result[0]?.id
-
-    if (!companyId) console.error('Company not found')
-
     await queryRunner.query(
-      `DELETE FROM ${companySettingsTable} WHERE  "companyId" = ${companyId}`,
-    )
-    await queryRunner.query(
-      `DELETE FROM ${companyTable} WHERE id = ${companyId}`,
+      `DELETE FROM ${companyTable} WHERE rfc = '${companyRFC}'`,
     )
   }
 }
