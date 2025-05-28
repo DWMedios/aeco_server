2e19616c0891e50b469552450eb9a0773b86c396
franciscocamposchimal <camposfranciscoing@gmail.com>
2025-05-28 12:44:14 -0600

    fix: delete aeco

--- src/aecos/app/delete-aeco.service.ts
+++ src/aecos/app/delete-aeco.service.ts
@@ -3,6 +3,7 @@ import {
   AECO_REPOSITORY,
   type IAecoRepository,
 } from '@shared/domain/repositories'
+import { AecoStatusEnum } from '@common/domain/enums/AecoStatus.enum'
 import type { IDeleteAecoService } from '@aecos/domain/services/IDeleteAecoService'
 
 @Injectable()
@@ -18,6 +19,15 @@ export class DeleteAecoService implements IDeleteAecoService {
     const aeco = await this.aecoRepository.findBy({ id })
 
     if (!aeco) throw new NotFoundException('El Aeco no existe')
+
+    await this.aecoRepository.updateById(id, {
+      folio: `${aeco.folio}-deleted-${Number(new Date())}`,
+      serialNumber: `${aeco.serialNumber}-deleted-${Number(new Date())}`,
+      isOnline: false,
+      initialSetup: false,
+      needsUpdate: false,
+      status: AecoStatusEnum.DEACTIVATED,
+    })
 
     const isDeleted = await this.aecoRepository.softDelete(id)
 
